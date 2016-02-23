ad7d3c30a938bb4ffbe7823003e1298def45e944
nikepupu <rangong@ucdavis.edu>
2016-02-23 13:44:37 -0800

    update

--- codeforce/5/D.cpp
+++ codeforce/5/D.cpp
@@ -15,29 +15,69 @@ template<typename T> inline bool chkmin(T &a, const T &b) { return a > b ? a = b
 template<typename T> inline bool chkmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
 
 using namespace std;
-#define mod %1000000007
-char a[2001];
-char b[2001];
-int m;
-int d;
-long long count=0;
+const long long mod = 1000000000+7;
+long long dp[2010][2010][2];// cur i, module,
+string a, b;
+int m,len,d;
+
+long long solve(string n)
+{
+
+	memset(dp, 0, sizeof(dp) );
+	dp[0][0][1] = 1;
+
+	for(int i = 0; i < len; i++)
+		for(int j = 0; j < m; j++)
+			for(int k =0; k <2; k++)
+				for(int p = 0; p <= (k? int(n[i]-'0'): 9); p++)
+				{	
+					if((i %2 == 1) && p != d) continue;
+					if((i % 2 == 0) && p == d) continue;
+					//if(!i && !p) continue;
+					int ni = i+1;
+					int nj = (10*j + p)%m;
+					int nk = (k && (p == int(n[i]-'0') ));
+					dp[ni][nj][nk] += dp[i][j][k];
+					dp[ni][nj][nk] %=mod;
+				}
+			
+	
+
+	 
+	return dp[len][0][1] + dp[len][0][0];
+}
+
+int good(string s)
+{
+	int a = 0;
+	for(int i = 0; i < len; i++)
+	{
+		if(i %2 == 0 && (s[i]-'0' == d) ) return 0;
+		if(i %2 == 1 && (s[i]-'0' != d) ) return 0;
+
+		a = (a * 10 + s[i]-'0')%m;
+		a %= m;
+	}
+	
+	if (a == 0)
+		return 1;
+
+	else return 0;
+}
+
+
 int main()
 {
 	cin >> m >> d;
-	scanf("%s", a);
-	scanf("%s", b);
-	int len = strlen(a);
+	cin >> a >> b;
+	len = a.length();
 
-	while(strcmp(a,b))
+	long long temp = (solve(b)-solve(a)+good(a))%mod;
+	while(temp <0)
 	{
-
-		for(int i = len-1; i >=0; i-- )
-		{
-
-		}
+		temp += mod;
 	}
-
-  
+	cout << temp << endl;
 
   return 0;
 }
---
+++ others/count.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <iomanip>
+
+template<typename T> inline bool chkmin(T &a, const T &b) { return a > b ? a = b, 1 : 0; }
+template<typename T> inline bool chkmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+using namespace std;
+
+string a;
+string b;
+long long dp[100][10][2];//长度， 数字，
+long long res[10]; 
+
+void check(string &s)
+{
+	for(int i = 0; i < s.length(); i++)
+		res[(s[i] - '0')]++;
+}
+
+void solve(string &s, int type)
+{
+ memset(dp, 0, sizeof(dp));
+ dp[0][0][1] = 1;
+ //for(int i = 0; i  < s[0]-'0'; i++)
+ 	//dp[0][i][0]++;
+
+ int len = s.length();
+
+ for(int i = 0; i <len; i++)
+ 	for(int j = 0; j <10; j++)
+ 		for(int k = 0; k < 2; k++)
+ 			for(int p =0; p <= (k? s[i]-'0': 9); p++)
+ 			{	
+ 				//if (!i &&!p) continue;
+ 				int ni = i+1;
+ 				int nj = p;
+ 				int nk = (k && (p == int(s[i] - '0')));
+ 				dp[ni][nj][nk] += dp[i][j][k];
+ 				//cout << dp[1][0][0]<<endl;
+ 			}
+
+ 	if (type == 1)
+	for(int j = 0; j < 10; j++)
+	{
+		//cout << dp[0][j][0]<<" ";	
+		res[j] += dp[len][j][0]+dp[len][j][1];
+	}
+
+	else{
+
+		for(int j = 0; j < 10; j++)
+		{
+			for(int k = 0; k < 2; k++)
+				res[j] -= dp[len][j][k];
+		}
+	}
+
+  
+}
+int main()
+{
+	//memset(a,'0',sizeof(a));
+	memset(res,0,sizeof(res));
+
+	cin >> a >>b;
+	
+
+
+	solve(b,1);
+
+	solve(a,2);
+	check(a);
+	
+  	for(int i = 0; i < 10; i++)
+  		cout << res[i] <<" ";
+  	cout << endl;
+
+  return 0;
+}
